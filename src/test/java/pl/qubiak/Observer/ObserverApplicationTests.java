2311e660da975b0b4194a5b031d8ddcdfecafa01
qubiak <kubiak1986@gmail.com>
2021-03-02 10:06:06 +0100

    init

---
+++ src/test/java/pl/qubiak/Observer/ObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.qubiak.Observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
